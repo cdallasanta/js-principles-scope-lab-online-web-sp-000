14072d4df3d0e183c87e8cefc4377d77b15f966b
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-24 20:06:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(name) {
+  customerName = name.toUpperCase();
+}
+
+function setBestCustomer(){
+  var bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(name) {
+  bestCustomer = name;
+}
+
+const leastFavoriteCustomer = "bob";
+
+function changeFavoriteCustomer(name) {
+  leastFavoriteCustomer = name;
+}
